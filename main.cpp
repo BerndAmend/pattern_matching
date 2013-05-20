3aaa9c254ebae9f929646aee69f7f8835b56d6e8
Bernd Amend <bernd.amend@googlemail.com>
2013-05-20 16:48:04 +0200

    add Boost Software License 1.0 and required headers

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * @author    Bernd Amend <berndamend@gemail.com>
+ * @date      2013
+ * @copyright Copyright Bernd Amend 2013
+ *            Distributed under the Boost Software License, Version 1.0.
+ *            (See accompanying file LICENSE_1_0.txt or copy at
+ *             http://www.boost.org/LICENSE_1_0.txt)
+ */
 #include <iostream>
 #include <vector>
 #include "pattern_matching.hpp"
--- pattern_matching.hpp
+++ pattern_matching.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file
+ * @author    Bernd Amend <berndamend@gemail.com>
+ * @date      2013
+ * @copyright Copyright Bernd Amend 2013
+ *            Distributed under the Boost Software License, Version 1.0.
+ *            (See accompanying file LICENSE_1_0.txt or copy at
+ *             http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 #pragma once
 
 #include <functional>
